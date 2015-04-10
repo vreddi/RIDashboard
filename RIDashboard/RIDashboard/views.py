704ec911a51a5ccd6ac906c75cb3f82cf4631931
Vishrut Reddi <vishrutreddi@gmail.com>
2015-04-10 02:30:27 -0500

    Revision 1
    * Learning through
    * Settled v1 of Front End

---
+++ RIDashboard/RIDashboard/views.py
@@ -0,0 +1,20 @@
+'''
+Function Based View
+
+__author__ = Vishrut Reddi
+'''
+
+from django.shortcuts import render
+
+
+
+'''
+Request Will be made from urls.py.
+
+@param request
+'''
+def home(request):
+
+	context = {}
+	template = "home.html"
+	return render(request, template, context)
